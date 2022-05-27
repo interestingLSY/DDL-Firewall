54d31466585aa43dd66afc7f44ae1d4799c2ef0e
BFTree <303664794@qq.com>
2022-05-27 23:48:00 +0800

    first commit

--- mainwindow.h
+++ mainwindow.h
@@ -9,13 +9,13 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	MainWindow(QWidget *parent = nullptr);
-	~MainWindow();
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
 private:
-	Ui::MainWindow *ui;
+    Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
